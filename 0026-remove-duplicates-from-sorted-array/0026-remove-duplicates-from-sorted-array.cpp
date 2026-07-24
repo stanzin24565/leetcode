05be04a53e74a97d822eb612e5116502084f166a
stanzin chondol <stanzin24565@iiitd.ac.in>
2026-07-24 16:17:48 +0530

    Time: 47 ms (5.49%), Space: 22.6 MB (81.27%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         if (nums.empty()) return 0;
-        int k = 0;
+        
         int i =1;
         while(i< nums.size()){
             if(nums[i -1]== nums[i]){
                 nums.erase(nums.begin() + i);
-                k++;
+                
             }
             else {
                 i++;
