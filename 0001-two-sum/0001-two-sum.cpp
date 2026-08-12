66db7198914432a658ad7af996c2773c09003bdf
stanzin chondol <stanzin24565@iiitd.ac.in>
2026-08-12 10:42:07 +0530

    Time: 3 ms (66.29%), Space: 15 MB (12.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> mp;
-        for(int i =0; i<= nums.size()-1 ; i++){
-            int compliment = target - nums[i];
-            if(mp.find(compliment) != mp.end()){
-                return {mp[compliment], i};
-                
+        for(int i=0;i<=nums.size()-1;i++){
+            int needed = target - nums[i];
+            if(mp.find(needed)!= mp.end()){
+                return {mp[needed],i};
             }
-            mp[nums[i]] = i;
-        } 
-        return {-1,-1};       
+            mp[nums[i]]= i;
+        }
+        return {-1,-1};
     }
+
 };
