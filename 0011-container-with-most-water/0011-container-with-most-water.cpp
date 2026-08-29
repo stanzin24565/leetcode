2169ae344b5b50684a4e07c7909c9886ac043f8a
stanzin chondol <stanzin24565@iiitd.ac.in>
2026-08-29 11:19:42 +0530

    Time: 4 ms (20.59%), Space: 63 MB (11.06%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left =0;
+        int right= height.size()-1;
+        int maxarea=0;
+
+        while(left<right){
+            int h = min(height[left], height[right]);
+            int w = right-left;
+
+            int area = h * w;
+            maxarea = max(maxarea,area);
+
+            if(height[left]< height[right]){
+                left++;
+
+            }
+            else{
+                right--;
+            }
+        }
+        return maxarea;
+         
+    }
+};
